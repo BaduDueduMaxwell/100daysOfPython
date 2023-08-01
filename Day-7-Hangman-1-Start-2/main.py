c10e804db62a2dad38eec8d3207567dd92be9663
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2023-08-01 22:08:57 +0000

    day 07

--- Day-7-Hangman-1-Start-2/main.py
+++ Day-7-Hangman-1-Start-2/main.py
@@ -68,28 +68,6 @@ lives = 6
 
 display = ["_" for _ in chosen_word]
 
-# while lives > 0 and "_" in display:
-#     user_guess = input("Guess the word: ").lower()
-#     found = False
-
-#     for i, letter in enumerate(chosen_word):
-#         if letter == user_guess:
-#             display[i] = user_guess
-#             found = True
-
-#     if found:
-#         print("You won!")
-
-#     if not found:
-#         lives -= 1
-
-#     print(stages[lives])
-#     print(" ".join(display))
-
-# if lives == 0:
-#     print('You lose!')
-
-
 while lives > 0 and "_" in display:
     user_guess = input("Guess the word: ").lower()
     found = False
